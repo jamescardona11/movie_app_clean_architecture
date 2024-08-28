22f2dbc702bca815bdf3493a110d1012f70c0f3e
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-08-28 08:43:27 -0500

    Create base client class

---
+++ lib/core/data/remote/base_http_client.dart
@@ -0,0 +1,22 @@
+import 'http_request_endpoint.dart';
+import 'http_result.dart';
+
+abstract class BaseHttpClient {
+  Future<HttpResult> createRequest(HttpRequestEndpoint request);
+
+  Duration get timeout => const Duration(seconds: 3);
+}
+
+bool isSuccessRequest(int? statusCode) =>
+    statusCode != null &&
+    ![
+      400, // Bad Request
+      401, // Unauthorized
+      402, // Payment Required
+      403, // Forbidden
+      404, // Not Found
+      405, // Method Not Allowed,
+      413, // Request Entity Too Large
+      414, // Request URI Too Long,
+      415, // Unsupported Media Type
+    ].contains(statusCode);
