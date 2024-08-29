a5230a5408189a1f9a97305015b0060f620b7037
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-08-28 20:21:23 -0500

    create base for repository

---
+++ lib/data/genres/genre_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:movie_app_clean_architecture/core/data/local/database/local_datasource_mixin.dart';
+import 'package:movie_app_clean_architecture/core/data/remote/base_http_client.dart';
+import 'package:movie_app_clean_architecture/domain/entities/genre_entity.dart';
+import 'package:movie_app_clean_architecture/domain/repository/genre_repository.dart';
+import 'package:sembast/sembast.dart';
+
+final class GenreRepositoryImpl with LocalDataSourceMixin implements GenreRepository {
+  @override
+  final Database database;
+  final BaseHttpClient httpClient;
+
+  GenreRepositoryImpl(this.database, this.httpClient);
+
+  @override
+  String get tableName => 'genres';
+
+  @override
+  Future<void> fetchGenres() async {}
+
+  @override
+  Future<void> updateOrCreate(List<GenreEntity> genres) {
+    // TODO: implement upsert
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<List<GenreEntity>> watchGenres() {
+    // TODO: implement watchGenres
+    throw UnimplementedError();
+  }
+}
--- lib/domain/repository/genre_repository.dart
+++ lib/domain/repository/genre_repository.dart
@@ -1,11 +1,9 @@
-import '../entities/genre_entity.dart';
+import 'package:movie_app_clean_architecture/domain/entities/genre_entity.dart';
 
 abstract interface class GenreRepository {
   Future<void> fetchGenres();
 
   Stream<List<GenreEntity>> watchGenres();
 
-  Future<void> upsert(GenreEntity genre);
-
-  Future<void> deleteGenre(int id);
+  Future<void> updateOrCreate(List<GenreEntity> genres);
 }
