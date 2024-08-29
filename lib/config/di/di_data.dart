f62c917a1d2ab57f6899d5136b2e47cae8bb4094
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-08-29 07:46:51 -0500

    Update DI modules

--- lib/config/di/di_data.dart
+++ lib/config/di/di_data.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
 import 'package:movie_app_clean_architecture/core/data/local/connectivity_provider_impl.dart';
+import 'package:movie_app_clean_architecture/core/data/local/database/sembast_db_init.dart';
 import 'package:movie_app_clean_architecture/core/data/local/preferences/app_shared_preferences_impl.dart';
 import 'package:movie_app_clean_architecture/core/data/local/preferences/local_shared_preferences_adapter.dart';
 import 'package:movie_app_clean_architecture/core/data/remote/base_http_client.dart';
@@ -11,12 +12,21 @@ import 'package:movie_app_clean_architecture/core/domain/repository/app_shared_p
 import 'package:movie_app_clean_architecture/core/domain/repository/connectivity_provider.dart';
 import 'package:movie_app_clean_architecture/data/genres/genre_repository_impl.dart';
 import 'package:movie_app_clean_architecture/domain/repository/genre_repository.dart';
+import 'package:sembast/sembast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class DataModule {
+  @singleton
+  @preResolve
+  Future<SharedPreferences> sharedPreferences() => SharedPreferences.getInstance();
+
   @injectable
-  LocalSharedPreferencesAdapter prefs(SharedPreferences sharedPreferences) => LocalSharedPreferencesAdapter(sharedPreferences);
+  LocalSharedPreferencesAdapter prefs(SharedPreferences preferences) => LocalSharedPreferencesAdapter(preferences);
+
+  @singleton
+  @preResolve
+  Future<Database> sembastDBApp() => SembastDBInit.initSembast();
 
   @Injectable(as: AppSharedPreferences)
   AppSharedPreferencesImpl get appSharedPreferences;
@@ -24,13 +34,13 @@ abstract class DataModule {
   @singleton
   BaseHttpClient httpClient() => AppHttpClient(http.Client());
 
-  @Singleton(as: ConnectivityProvider, dispose: disposeProvider)
+  @Singleton(as: ConnectivityProvider, dispose: disposeDependency)
   ConnectivityProviderImpl get connectivityProvider;
 
   @Singleton(as: GenreRepository)
   GenreRepositoryImpl get genreRepository;
 }
 
-FutureOr<void> disposeProvider(ConnectivityProvider provider) {
+FutureOr<void> disposeDependency(ConnectivityProvider provider) {
   unawaited(provider.close());
 }
