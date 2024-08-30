e59d327e3999a7f1699f2b935a73fc7770a1f796
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-08-30 00:08:48 -0500

    Update Splash page

--- lib/presentation/pages/intro/splash_page.dart
+++ lib/presentation/pages/intro/splash_page.dart
@@ -1,86 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:movie_app_clean_architecture/config/di/di.dart';
-import 'package:movie_app_clean_architecture/config/navigation/navigation_service.dart';
 import 'package:movie_app_clean_architecture/config/res/res.dart';
-import 'package:movie_app_clean_architecture/core/provider/moc_builder.dart';
-import 'package:movie_app_clean_architecture/core/provider/moc_provider.dart';
 import 'package:movie_app_clean_architecture/core/widgets/app_icon.dart';
 
-import 'controller/intro_controller.dart';
-import 'controller/intro_state.dart';
-
-class SplashPage extends StatefulWidget {
+class SplashPage extends StatelessWidget {
   const SplashPage({
     super.key,
   });
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
-  late AnimationController controller;
-  bool determinate = false;
-
-  @override
-  void initState() {
-    controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    )..addListener(() {
-        setState(() {});
-      });
-    controller.repeat();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MocProvider(
-      create: (context) => getIt<IntroController>()..init(),
-      child: MocBuilder<IntroController, IntroState>(
-        listener: (moc, state) {
-          if (!state.isLoading) {
-            // goToHome();
-          }
-        },
-        builder: (moc, state) => Scaffold(
-          body: Center(
-            child: SizedBox(
-              child: Column(
-                children: [
-                  const Spacer(),
-                  const Padding(
-                    padding: EdgeInsets.only(top: Dimens.size60, bottom: Dimens.size12),
-                    child: AppIcon(size: Dimens.size60),
-                  ),
-                  Text(
-                    'Moc App',
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  const Spacer(),
-                  SizedBox(
-                    width: 120,
-                    child: Lottie.asset(AppAssets.lottieLoader),
-                  ),
-                  const SizedBox(height: Dimens.size60),
-                ],
-              ),
+    return Center(
+      child: SizedBox(
+        child: Column(
+          children: [
+            const Spacer(),
+            const Padding(
+              padding: EdgeInsets.only(top: Dimens.size60, bottom: Dimens.size12),
+              child: AppIcon(size: Dimens.size60),
             ),
-          ),
+            Text(
+              'Moc App',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const Spacer(),
+            SizedBox(
+              width: 120,
+              child: Lottie.asset(AppAssets.lottieLoader),
+            ),
+            const SizedBox(height: Dimens.size60),
+          ],
         ),
       ),
     );
-  }
-
-  void goToHome() {
-    NavigationService().goTo(NavigationService.home);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
   }
 }
