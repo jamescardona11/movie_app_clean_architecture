d2ffd4cdc985132e6a34a441e84de9989a395ffb
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-08-30 00:04:29 -0500

    assets class

--- lib/config/res/app_assets.dart
+++ lib/config/res/app_assets.dart
@@ -1,3 +1,5 @@
 abstract class AppAssets {
-  static const String logo = 'assets/logo.png';
+  static const String introImage1 = 'assets/intro1.png';
+  static const String introImage2 = 'assets/intro2.png';
+  static const String lottieLoader = 'assets/loader.json';
 }
