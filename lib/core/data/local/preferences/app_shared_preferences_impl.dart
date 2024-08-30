359941e59cfbb9bfdd8d3b64ccc686ccdb6e3e55
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-08-30 00:41:44 -0500

    Update preferences

--- lib/core/data/local/preferences/app_shared_preferences_impl.dart
+++ lib/core/data/local/preferences/app_shared_preferences_impl.dart
@@ -8,27 +8,44 @@ class AppSharedPreferencesImpl implements AppSharedPreferences {
   const AppSharedPreferencesImpl(this._localSharedPreferencesAdapter);
 
   static const String _firstTimeKey = 'first_time';
-  static const String _movieItemsViewIsListKey = 'movie_items_view_is_list';
-  static const String _lastDataOldKey = 'last_data_old';
+  static const String _lastPopularOldKey = 'last_popular_old';
+  static const String _lastNowPlayingOldKey = 'last_now_playing_old';
+  static const String _nowPlayingGridViewKey = 'now_playing_grid_view';
+  static const String _popularGridViewKey = 'popular_grid_view';
 
   @override
   Future<bool> get isFirstTime => _localSharedPreferencesAdapter.readBool(_firstTimeKey).then((value) => value ?? true);
 
   @override
-  Future<bool> get movieItemsViewIsList => _localSharedPreferencesAdapter.readBool(_movieItemsViewIsListKey).then((value) => value ?? true);
-
-  @override
   Future<void> setFirstTimeDone() => _localSharedPreferencesAdapter.save(_firstTimeKey, false);
 
   @override
-  Future<void> changeMovieItemView() async {
-    final isList = await movieItemsViewIsList;
-    await _localSharedPreferencesAdapter.save(_movieItemsViewIsListKey, !isList);
+  Future<bool> get isNowPlayingOld => _localSharedPreferencesAdapter.readBool(_lastNowPlayingOldKey).then((value) => value ?? false);
+
+  @override
+  Future<bool> get isPopularOld => _localSharedPreferencesAdapter.readBool(_lastPopularOldKey).then((value) => value ?? false);
+
+  @override
+  Future<bool> get nowPlayingGridView => _localSharedPreferencesAdapter.readBool(_nowPlayingGridViewKey).then((value) => value ?? false);
+
+  @override
+  Future<bool> get popularGridView => _localSharedPreferencesAdapter.readBool(_popularGridViewKey).then((value) => value ?? false);
+
+  @override
+  Future<void> setLastDataOld() => _localSharedPreferencesAdapter.save(_lastNowPlayingOldKey, true);
+
+  @override
+  Future<void> setLastPopularOld() => _localSharedPreferencesAdapter.save(_lastPopularOldKey, true);
+
+  @override
+  Future<void> setNowPlayingGridView() async {
+    final isGrid = await nowPlayingGridView;
+    await _localSharedPreferencesAdapter.save(_nowPlayingGridViewKey, !isGrid);
   }
 
   @override
-  Future<void> get isLastDataOld => _localSharedPreferencesAdapter.readBool(_lastDataOldKey).then((value) => value ?? false);
-
-  @override
-  Future<void> setLastDataOld() => _localSharedPreferencesAdapter.save(_lastDataOldKey, true);
+  Future<void> setPopularGridView() async {
+    final isGrid = await popularGridView;
+    await _localSharedPreferencesAdapter.save(_popularGridViewKey, !isGrid);
+  }
 }
--- lib/core/domain/repository/app_shared_preferences.dart
+++ lib/core/domain/repository/app_shared_preferences.dart
@@ -2,9 +2,15 @@ abstract interface class AppSharedPreferences {
   Future<bool> get isFirstTime;
   Future<void> setFirstTimeDone();
 
-  Future<bool> get movieItemsViewIsList;
-  Future<void> changeMovieItemView();
-
-  Future<void> get isLastDataOld;
+  Future<bool> get isNowPlayingOld;
   Future<void> setLastDataOld();
+
+  Future<bool> get isPopularOld;
+  Future<void> setLastPopularOld();
+
+  Future<bool> get nowPlayingGridView;
+  Future<void> setNowPlayingGridView();
+
+  Future<bool> get popularGridView;
+  Future<void> setPopularGridView();
 }
